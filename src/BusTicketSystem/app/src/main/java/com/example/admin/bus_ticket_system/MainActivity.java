c957109fec1f6eaae4cdbec4193f495e1c810d38
PatrickA7 <patrick.alegria7@gmail.com>
2018-09-13 21:18:30 +0930

    Route 2 link to map

--- src/BusTicketSystem/app/src/main/java/com/example/admin/bus_ticket_system/MainActivity.java
+++ src/BusTicketSystem/app/src/main/java/com/example/admin/bus_ticket_system/MainActivity.java
@@ -1,16 +1,39 @@
 package com.example.admin.bus_ticket_system;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    public int a= 1;
+    Button button;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Get the view from activity_main.xml
         setContentView(R.layout.activity_main);
+
+        // Locate the button in activity_main.xml
+        button = (Button) findViewById(R.id.route2);
+
+        // Capture button clicks
+        button.setOnClickListener(new OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(MainActivity.this,
+                        MapsActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
+
 }
