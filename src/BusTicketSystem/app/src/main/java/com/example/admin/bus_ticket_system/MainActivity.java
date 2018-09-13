b115df8b4fcb922a779813052f895ed927ffdbe4
PatrickA7 <patrick.alegria7@gmail.com>
2018-09-13 21:49:32 +0930

    Connect all route buttons to map page

--- src/BusTicketSystem/app/src/main/java/com/example/admin/bus_ticket_system/MainActivity.java
+++ src/BusTicketSystem/app/src/main/java/com/example/admin/bus_ticket_system/MainActivity.java
@@ -13,7 +13,11 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
 
-    Button button;
+    Button buttonRoute2;
+    Button buttonRoute3;
+    Button buttonRoute4;
+    Button buttonRoute9;
+    Button buttonRoute12;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,10 +26,50 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Locate the button in activity_main.xml
-        button = (Button) findViewById(R.id.route2);
+        buttonRoute2 = (Button) findViewById(R.id.route2);
+        buttonRoute3 = (Button) findViewById(R.id.route3);
+        buttonRoute4 = (Button) findViewById(R.id.route4);
+        buttonRoute9 = (Button) findViewById(R.id.route9);
+        buttonRoute12 = (Button) findViewById(R.id.route12);
 
         // Capture button clicks
-        button.setOnClickListener(new OnClickListener() {
+        buttonRoute2.setOnClickListener(new OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(MainActivity.this,
+                        MapsActivity.class);
+                startActivity(myIntent);
+            }
+        });
+        buttonRoute3.setOnClickListener(new OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(MainActivity.this,
+                        MapsActivity.class);
+                startActivity(myIntent);
+            }
+        });
+        buttonRoute4.setOnClickListener(new OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(MainActivity.this,
+                        MapsActivity.class);
+                startActivity(myIntent);
+            }
+        });
+        buttonRoute9.setOnClickListener(new OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(MainActivity.this,
+                        MapsActivity.class);
+                startActivity(myIntent);
+            }
+        });
+        buttonRoute12.setOnClickListener(new OnClickListener() {
             public void onClick(View arg0) {
 
                 // Start NewActivity.class
